cb2ba026528169b064249528fd688e72d4ea36e7
Rahat393 <rahataminulislam37@gmail.com>
2023-08-07 16:41:07 +0600

    authentication implement

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDih9G9fTLD37vvtIaGokAIAHpGr6jNHdE",
+  authDomain: "aircnc-24afa.firebaseapp.com",
+  projectId: "aircnc-24afa",
+  storageBucket: "aircnc-24afa.appspot.com",
+  messagingSenderId: "956242492109",
+  appId: "1:956242492109:web:a1e16e9a50821f89c20784"
+};
+
+const app = initializeApp(firebaseConfig)
+
+export default app
